0ab1147aa9c340da24c0c1e8ecb7dc66c5c908b2
sertoriobanze <111365373+sertoriobanze@users.noreply.github.com>
2022-09-28 06:00:48 -0700

    first commit

---
+++ painel-control.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+    <link rel="stylesheet" href="style.css">
+    <title>Painel-de-control</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        <div class="logo">
+            <img src="./public-img/logo.png" alt="logo" class="img">
+        </div>
+    </div>
+
+   <div class="dashbord">
+        <div class="menu">
+        <div class="card">
+            <div class="card-img">
+                <img src="./public-img/icons/upload.png" alt=" icon" class="icon">
+            </div>
+            <div class="title-card">
+                <p>Publicar Contedudo na Pagina Principal</p>
+            </div>
+            
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="./public-img/icons/upload.png" alt=" icon" class="icon">
+            </div>
+            <div class="title-card">
+                <p>Publicar Contedudo na Pagina Principal</p>
+            </div>
+            
+        </div>  
+        <div class="card">
+            <div class="card-img">
+                <img src="./public-img/icons/upload.png" alt=" icon" class="icon">
+            </div>
+            <div class="title-card">
+                <p>Publicar Conteudo Na Pagina Informacoes</p>
+            </div>
+            
+        </div>  
+        <div class="card">
+            <div class="card-img">
+                <img src="./public-img/icons/upload.png" alt=" icon" class="icon">
+            </div>
+            <div class="title-card">
+                <p>Listar bolsistas</p>
+            </div>
+            
+        </div>  
+           
+
+        </div>
+        <div class="fuctions">
+        
+        </div>
+   </div> 
+</div>
+    
+</body>
+</html>
